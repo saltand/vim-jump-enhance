0cf384bf1c5b7de49b86fd839e95c784da10da37
saltand <u6a69a9@outlook.com>
2025-08-15 23:43:37 +0800

    添加了新的命令“vim.enhance.jump”，用于在文本编辑器中跳转括号和匹配标签，同时更新了相关配置文件。

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
 import { defineExtension } from 'reactive-vscode'
-import { window } from 'vscode'
+import { commands, Range, window } from 'vscode'
 
 const { activate, deactivate } = defineExtension(() => {
-  window.showInformationMessage('Hello')
+  const cmd = commands.registerTextEditorCommand('vim.enhance.jump', async (_textEditor, _edit) => {
+    const editor = window.activeTextEditor
+    if (!editor)
+      return
+
+    const doc = editor.document
+    const pos = editor.selection.active
+
+    // 匹配括号
+    const charAt = doc.getText(new Range(pos, pos.translate(0, 1)))
+    const isBracket = '(){}[]'.includes(charAt)
+    if (isBracket) {
+      await commands.executeCommand('editor.action.jumpToBracket')
+      return
+    }
+
+    // 匹配标签
+    const supportedLanguages = ['html', 'xml', 'vue', 'javascriptreact', 'typescriptreact']
+    if (supportedLanguages.includes(doc.languageId)) {
+      await commands.executeCommand('editor.emmet.action.matchTag')
+    }
+  })
+
+  return () => {
+    cmd.dispose()
+  }
 })
 
 export { activate, deactivate }
